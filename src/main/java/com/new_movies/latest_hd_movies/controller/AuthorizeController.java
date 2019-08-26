743400ee2b5e7b52d6050c87e9e3a57487168eb0
Wesley-Yu <1633235633@qq.com>
2019-08-26 16:53:59 +0800

    首页

---
+++ src/main/java/com/new_movies/latest_hd_movies/controller/AuthorizeController.java
@@ -0,0 +1,26 @@
+package com.new_movies.latest_hd_movies.controller;
+import com.new_movies.latest_hd_movies.dto.AccessTokenDTO;
+import com.new_movies.latest_hd_movies.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("1c6819d3d068acda9639");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8090/callback");
+        githubProvider.getAccessToken(accessTokenDTO);
+        return "login";
+    }
+}
---
+++ src/main/java/com/new_movies/latest_hd_movies/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.new_movies.latest_hd_movies.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/login")
+    public String Index(){
+        return "login";
+    }
+}
