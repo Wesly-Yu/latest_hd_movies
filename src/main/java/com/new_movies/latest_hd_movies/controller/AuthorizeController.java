bad39331e5af8050e02f14ee9295c4d3db1fdf24
Wesley-Yu <1633235633@qq.com>
2019-08-26 17:46:25 +0800

    demo run--002

--- src/main/java/com/new_movies/latest_hd_movies/controller/AuthorizeController.java
+++ src/main/java/com/new_movies/latest_hd_movies/controller/AuthorizeController.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-//@Controller
-//public class AuthorizeController {
-//
-//    @Autowired
-//    private GithubProvider githubProvider;
-//    @GetMapping("/callback")
-//    public String callback(@RequestParam(name="code") String code,
-//                           @RequestParam(name = "state") String state){
-//        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-//        accessTokenDTO.setCode(code);
-//        accessTokenDTO.setClient_id("1c6819d3d068acda9639");
-//        accessTokenDTO.setState(state);
-//        accessTokenDTO.setRedirect_uri("http://localhost:8090/callback");
-//        githubProvider.getAccessToken(accessTokenDTO);
-//        return "login";
-//    }
-//}
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("1c6819d3d068acda9639");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8090/callback");
+        githubProvider.getAccessToken(accessTokenDTO);
+        return "login";
+    }
+}
--- src/main/java/com/new_movies/latest_hd_movies/controller/IndexController.java
+++ src/main/java/com/new_movies/latest_hd_movies/controller/IndexController.java
@@ -2,12 +2,12 @@ package com.new_movies.latest_hd_movies.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//
-//@Controller
-//public class IndexController {
-//
-//    @GetMapping("/login")
-//    public String Index(){
-//        return "login";
-//    }
-//}
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/login")
+    public String Index(){
+        return "login";
+    }
+}
