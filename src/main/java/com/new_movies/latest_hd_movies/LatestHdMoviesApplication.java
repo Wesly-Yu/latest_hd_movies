dba6eb1116de9034a71c523c320507846d279467
Wesley-Yu <1633235633@qq.com>
2019-08-26 15:25:15 +0800

    Initial commit

---
+++ src/main/java/com/new_movies/latest_hd_movies/LatestHdMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.new_movies.latest_hd_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestHdMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatestHdMoviesApplication.class, args);
+    }
+
+}
