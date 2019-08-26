aad9d9b5ac93e29a34f4c2e7f33a3701cbeaa01e
Wesly-Yu <1633235633@qq.com>
2019-08-26 22:49:09 +0800

    adjust--2

--- src/main/java/com/new_movies/latest_hd_movies/controller/AuthorizeController.java
+++ src/main/java/com/new_movies/latest_hd_movies/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package com.new_movies.latest_hd_movies.controller;
 import com.new_movies.latest_hd_movies.dto.AccessTokenDTO;
+import com.new_movies.latest_hd_movies.dto.GithubUser;
 import com.new_movies.latest_hd_movies.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +14,26 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.accessid}")
+    private String accessid;
+    @Value("${github.callbackurl}")
+    private String callbackurl;
+    @Value("${github.client_secret}")
+    private String client_secret;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("1c6819d3d068acda9639");
+        accessTokenDTO.setClient_id(accessid);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8090/callback");
-        githubProvider.getAccessToken(accessTokenDTO);
+        accessTokenDTO.setRedirect_uri(callbackurl);
+        accessTokenDTO.setClient_secret(client_secret);
+        String[] accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getUsername());
         return "login";
     }
 }
--- src/main/java/com/new_movies/latest_hd_movies/provider/GithubProvider.java
+++ src/main/java/com/new_movies/latest_hd_movies/provider/GithubProvider.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @Component
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+    public String[] getAccessToken(AccessTokenDTO accessTokenDTO){
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
         RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
@@ -21,12 +21,18 @@ public class GithubProvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-            } catch (IOException e) {
+            String[] split = string.split("&");
+            String tokenstr = split [0];
+            String[] token = tokenstr.split("=");
+            System.out.println("--------------");
+            System.out.println(token);
+            return token;
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
-    public GithubUser getUser(String accessToken){
+    public GithubUser getUser(String[] accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
             .url("https://api.github.com/user?access_token="+accessToken)
@@ -35,6 +41,7 @@ public class GithubProvider {
             Response response = client.newCall(request).execute();
             String string = response.body().string();
             GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            System.out.println(githubUser);
             return githubUser;
         }catch (IOException e){
         }
