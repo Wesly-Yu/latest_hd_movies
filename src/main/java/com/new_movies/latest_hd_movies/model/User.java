0004c7a86b26fa17b2f59b83615007ffee2c2adb
Wesley-Yu <1633235633@qq.com>
2019-08-30 14:16:02 +0800

    数据库参数配置与连接

--- src/main/java/com/new_movies/latest_hd_movies/model/User.java
+++ src/main/java/com/new_movies/latest_hd_movies/model/User.java
@@ -7,10 +7,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
+    //用户id
     private Integer id;
+    //用户名称
     private String name;
+    //用户帐户id
     private String accountId;
+    //用户的token
     private String token;
+    //创建时间
     private Long gmtCreate;
+    //修改时间
     private Long gmtModified;
 }
