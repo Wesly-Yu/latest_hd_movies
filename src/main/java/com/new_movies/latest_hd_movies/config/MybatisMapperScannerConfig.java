24e3a1fa227c1f48d1ec516d9e6045ba784135c2
Wesley-Yu <1633235633@qq.com>
2019-09-02 16:50:28 +0800

    spring boot创建数据库表格--002

--- src/main/java/com/new_movies/latest_hd_movies/config/MybatisMapperScannerConfig.java
+++ src/main/java/com/new_movies/latest_hd_movies/config/MybatisMapperScannerConfig.java
@@ -1,11 +1,14 @@
 package com.new_movies.latest_hd_movies.config;
 import org.springframework.beans.factory.annotation.Value;
 import	java.awt.Component;
+import java.io.IOException;
+import com.alibaba.druid.pool.DruidDataSource;
 
-
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
@@ -19,14 +22,38 @@ public class MybatisMapperScannerConfig {
     private String url;
 
     @Value("${spring.datasource.username}")
-    private String useraccount;
+    private String username;
 
     @Value("${spring.datasource.password}")
     private String password;
 
     @Bean
+    public PropertiesFactoryBean configProperties() throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        propertiesFactoryBean.setLocation(resolver.getResource("application.properties"));
+        return propertiesFactoryBean;
+    }
+
+    @Bean
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaxActive(30);
+        dataSource.setInitialSize(10);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTestOnBorrow(true);
+        return dataSource;
+    }
+
+    @Bean
     public DataSourceTransactionManager  dataSourceTransactionManager() {
         DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource());
+        return dataSourceTransactionManager;
 
     }
 
